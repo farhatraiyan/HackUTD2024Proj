2f996b44be104070b51bc01f18a6fddf01249e22
Farhat R. Kabir <148591416+farhatraiyan@users.noreply.github.com>
2024-09-21 18:15:17 -0500

    Initial Flask App commit

---
+++ api/app.py
@@ -0,0 +1,12 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def get_data():
+    data = {"message": "API is responsive."}
+    return jsonify(data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
