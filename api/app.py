3e80455edbdf7e75d46f497b5c86e472d6bf301f
Farhat R. Kabir <148591416+farhatraiyan@users.noreply.github.com>
2024-09-22 01:17:04 -0500

    Flask api update for supporting React

--- api/app.py
+++ api/app.py
@@ -1,11 +1,14 @@
 from flask import Flask, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
-
-@app.route('/', methods=['GET'])
+@app.route('/status', methods=['GET'])
 def get_data():
-    data = {"message": "API is responsive."}
+    data = {
+        'message': 'API is responsive!!'
+    }
     return jsonify(data)
 
 if __name__ == '__main__':
