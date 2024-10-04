68b1aed672f9b784d621aa6e719a0d557d7db26d
Farhat R. Kabir <148591416+farhatraiyan@users.noreply.github.com>
2024-10-04 03:20:30 -0500

    Update accounts.py

--- api/controllers/accounts.py
+++ api/controllers/accounts.py
@@ -15,28 +15,50 @@ class CreateAccount(Resource):
             return jsonify(new_account.to_dict())
         except Exception as e:
             db.session.rollback()
-            abort(500, description=str(e))
+            abort(500, description='Error: failed to create account.')
 
 class DeleteAccount(Resource):
-    def delete(self, username):
-        if not username:
+    def delete(self, id):
+        if not id:
             abort(400, description="Invalid input.")
         try:
-            account = Account.query.filter_by(username=username).first()
+            account = Account.query.filter_by(id=id).first()
             if account:
                 db.session.delete(account)
                 db.session.commit()
                 return jsonify(account.to_dict())
-            print('account not found')
             return {'message': f'Account not found'}, 404
         except Exception as e:
             db.session.rollback()
-            abort(500, description=str(e))
+            abort(500, description='Error: failed to delete account.')
 
 class ListAccounts(Resource):
     def get(self):
         try:
             accounts = Account.query.all()
             return jsonify([account.to_dict() for account in accounts])
+        except Exception as e:
+            abort(500, description=str(e))
+
+class UpdateAccount(Resource):
+    def put(self, id):
+        try:
+            account = Account.query.filter_by(id=id).first()
+
+            if not account:
+                return {'message': f'Account not found'}, 404
+
+            account_data = request.get_json()
+
+            if not account_data:
+                return {'message': 'No input data provided'}, 400
+
+            if 'username' in account_data:
+                account.username = account_data['username']
+            if 'password' in account_data:
+                account.password = account_data['password']
+
+            db.session.commit()
+            return jsonify(account.to_dict())
         except Exception as e:
             abort(500, description=str(e))
