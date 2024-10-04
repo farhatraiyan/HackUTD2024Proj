14f99330d63960a1908aaba42af68db58322ab88
Farhat R. Kabir <148591416+farhatraiyan@users.noreply.github.com>
2024-10-04 03:20:41 -0500

    Update apiTest.js

--- my-react-app/apiTest.js
+++ my-react-app/apiTest.js
@@ -1,6 +1,8 @@
 const serviceUrl = 'http://127.0.0.1:8101';
 
 describe('Flask API', () => {
+    let postedId;
+
     it('GET /accounts should return any empty array before any accounts are posted', async () => {
         const req = {
             method: 'GET'
@@ -27,16 +29,71 @@ describe('Flask API', () => {
         const response = await fetch(`${serviceUrl}/accounts`, req);
         const resBody = await response.json();
         console.log(resBody);
+        postedId = resBody.id;
     });
 
-    it('/accounts should delete the user3 account', async () => {
-        const username = "user3";
+    it('GET /accounts should now return posted user', async () => {
+        const req = {
+            method: 'GET'
+        }
 
+        const response = await fetch(`${serviceUrl}/accounts`, req);
+        const resBody = await response.json();
+
+        console.log(resBody);
+    });
+
+    it('POST /accounts should fail to post account with same username', async () => {
+        const account = {
+            username: 'user3',
+            password: 'pass'
+        }
+
+        const req = {
+            method: 'POST',
+            body: JSON.stringify(account),
+            headers: { 'Content-Type': 'application/json' }
+        }
+
+        const response = await fetch(`${serviceUrl}/accounts`, req);
+        const resBody = await response.json();
+        console.log(resBody);
+    });
+
+    it('PUT /accounts should update the account username (and password)', async () => {
+        const account = {
+            username: 'user4',
+            password: 'pass2'
+        }
+
+        const req = {
+            method: 'PUT',
+            body: JSON.stringify(account),
+            headers: { 'Content-Type': 'application/json' }
+        }
+
+        const response = await fetch(`${serviceUrl}/accounts/${postedId}`, req);
+        const resBody = await response.json();
+        console.log(resBody);
+    });
+
+    it('DELETE /accounts should delete the posted user account', async () => {
         const req = {
             method: 'DELETE'
         }
 
-        const response = await fetch(`${serviceUrl}/accounts/${username}`, req);
+        const response = await fetch(`${serviceUrl}/accounts/${postedId}`, req);
+        const resBody = await response.json();
+
+        console.log(resBody);
+    });
+
+    it('DELETE /accounts should fail to delete non-existant account', async () => {
+        const req = {
+            method: 'DELETE'
+        }
+
+        const response = await fetch(`${serviceUrl}/accounts/${postedId}`, req);
         const resBody = await response.json();
 
         console.log(resBody);
