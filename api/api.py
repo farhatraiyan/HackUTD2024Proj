b35ae5897532da5ddc4d3357242d0ecb7e115a49
Farhat R. Kabir <148591416+farhatraiyan@users.noreply.github.com>
2024-10-04 01:40:36 -0500

    Update api.py

--- api/api.py
+++ api/api.py
@@ -1,7 +1,15 @@
-from app import app
-from appDB import db
+from flask_restful import Api
+from appDB import create_app, db
+from controllers.accounts import CreateAccount, ListAccounts
+
+app = create_app()
+api = Api(app)
+
+with app.app_context():
+    db.create_all()
+
+api.add_resource(CreateAccount, '/accounts')
+api.add_resource(ListAccounts, '/accounts')
 
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
     app.run(debug=True, port=8101)
