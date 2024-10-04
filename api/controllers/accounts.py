ed3e64a180fb35ff4b789ec244c249a3f2085762
Farhat R. Kabir <148591416+farhatraiyan@users.noreply.github.com>
2024-10-04 03:34:25 -0500

    Update accounts.py

--- api/controllers/accounts.py
+++ api/controllers/accounts.py
@@ -3,16 +3,27 @@ from flask_restful import Resource
 from appDB import db
 from models.accounts import Account
 
+def retrieveAccount(params):
+    account = Account.query.filter_by(**params).first()
+
+    if account:
+        return account
+
+    return None
+
 class CreateAccount(Resource):
     def post(self):
         account_data = request.json
+
         if not account_data:
             abort(400, description="Invalid input.")
+
         try:
             new_account = Account(**account_data)
             db.session.add(new_account)
             db.session.commit()
             return jsonify(new_account.to_dict())
+
         except Exception as e:
             db.session.rollback()
             abort(500, description='Error: failed to create account.')
@@ -21,29 +32,45 @@ class DeleteAccount(Resource):
     def delete(self, id):
         if not id:
             abort(400, description="Invalid input.")
+
         try:
-            account = Account.query.filter_by(id=id).first()
+            account = retrieveAccount({'id': id})
+
             if account:
                 db.session.delete(account)
                 db.session.commit()
                 return jsonify(account.to_dict())
+
             return {'message': f'Account not found'}, 404
         except Exception as e:
             db.session.rollback()
-            abort(500, description='Error: failed to delete account.')
+            abort(500, description='Failed to delete account.')
 
 class ListAccounts(Resource):
     def get(self):
         try:
             accounts = Account.query.all()
+
             return jsonify([account.to_dict() for account in accounts])
         except Exception as e:
-            abort(500, description=str(e))
+            abort(500, description='Failed to retrieve accounts.')
+
+class RetrieveAccountById(Resource):
+    def get(self, id):
+        try:
+            account = retrieveAccount({'id': id})
+
+            if account:
+                return jsonify(account.to_dict())
+
+            return {'message': f'Account not found'}, 404
+        except Exception as e:
+            abort(500, description='Failed to retrieve account.')
 
 class UpdateAccount(Resource):
     def put(self, id):
         try:
-            account = Account.query.filter_by(id=id).first()
+            account = retrieveAccount({'id': id})
 
             if not account:
                 return {'message': f'Account not found'}, 404
@@ -61,4 +88,4 @@ class UpdateAccount(Resource):
             db.session.commit()
             return jsonify(account.to_dict())
         except Exception as e:
-            abort(500, description=str(e))
+            abort(500, description='Failed to update account.')
