b0119861280f98cbc7c566aecade47e82cd80a1b
Farhat R. Kabir <148591416+farhatraiyan@users.noreply.github.com>
2024-10-04 03:20:38 -0500

    Update api.py

--- api/api.py
+++ api/api.py
@@ -1,6 +1,6 @@
 from flask_restful import Api
 from appDB import create_app, db
-from controllers.accounts import CreateAccount, DeleteAccount, ListAccounts
+from controllers.accounts import CreateAccount, DeleteAccount, ListAccounts, UpdateAccount
 
 app = create_app()
 api = Api(app)
@@ -9,8 +9,9 @@ with app.app_context():
     db.create_all()
 
 api.add_resource(CreateAccount, '/accounts')
-api.add_resource(DeleteAccount, '/accounts/<string:username>')
+api.add_resource(DeleteAccount, '/accounts/<int:id>')
 api.add_resource(ListAccounts, '/accounts')
+api.add_resource(UpdateAccount, '/accounts/<int:id>')
 
 if __name__ == '__main__':
     app.run(debug=True, port=8101)
