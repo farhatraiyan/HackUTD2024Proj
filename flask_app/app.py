00f51407bc228f68512caac3bc8408641921e445
Farhat R. Kabir <148591416+farhatraiyan@users.noreply.github.com>
2024-10-05 07:52:01 -0500

    home rename, status page

--- flask_app/app.py
+++ flask_app/app.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template
 from flask_restful import Api
 from appDB import create_app, db
 from controllers.accounts import CreateAccount, DeleteAccount, ListAccounts, RetrieveAccountById, RetrieveAccountByUsername, UpdateAccount
-from controllers.utils import Status
 
 app = create_app()
 api = Api(app)
@@ -17,11 +16,13 @@ api.add_resource(RetrieveAccountById, '/accounts/<int:id>')
 api.add_resource(RetrieveAccountByUsername, '/accounts/<string:username>')
 api.add_resource(UpdateAccount, '/accounts/<int:id>')
 
-api.add_resource(Status, '/status')
-
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('home.html')
+
+@app.route('/status')
+def status():
+    return render_template('status.html')
 
 if __name__ == '__main__':
     app.run(debug=True, port=8101)
---
+++ flask_app/static/js/scripts/status.js
@@ -0,0 +1,1 @@
+ReactDOM.render(<Navbar currentPage={'status'} />, document.getElementById('navbar'));
