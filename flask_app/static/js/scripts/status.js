ed8f7321e6d74a687be23784615c4e1f6f2b0387
Farhat R. Kabir <148591416+farhatraiyan@users.noreply.github.com>
2024-10-05 08:06:02 -0500

    Update status.js

--- flask_app/static/js/scripts/status.js
+++ flask_app/static/js/scripts/status.js
@@ -1,1 +1,16 @@
+function checkStatus() {
+    fetch('/api/status')
+        .then(response => response.json())
+        .then(data => {
+            if (data['status'] == 'ok') {
+                document.getElementById('status').innerHTML = 'All systems are operational.';
+                document.getElementById('status').style.color = 'green';
+            } else {
+                document.getElementById('status').innerHTML = 'There is an issue with the system. Please try again later.';
+                document.getElementById('status').style.color = 'red';
+            }
+        });
+}
+
 ReactDOM.render(<Navbar currentPage={'status'} />, document.getElementById('navbar'));
+ReactDOM.render(<StatusDashboard checkStatus={checkStatus} />, document.getElementById('statusDashboard'));
