8248ce8a6c11613eb834d4cb1122b72f96465d06
Farhat R. Kabir <148591416+farhatraiyan@users.noreply.github.com>
2024-10-06 06:36:44 -0500

    views directory

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -4,9 +4,9 @@ function Navbar() {
     return (
         <ul>
             <li><a href = "/">Home</a></li>
-            <li><a href = "/src/create/">Create Account</a></li>
-            <li><a href = "/src/status/">Status</a></li>
-            <li><a href = "/src/about/">About</a></li>
+            <li><a href = "/src/views/create/">Create Account</a></li>
+            <li><a href = "/src/views/status/">Status</a></li>
+            <li><a href = "/src/views/about/">About</a></li>
         </ul>
     );
 }
