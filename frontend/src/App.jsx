98f1181a2fe77ea56fc715472f06f51dfcae1289
sudinraj <sudinrajbanshi@gmail.com>
2024-10-05 23:37:23 -0500

    vite flask setup
    made frontend folder to connect react with flask

---
+++ frontend/src/App.jsx
@@ -0,0 +1,31 @@
+import './App.css';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+
+function App() {
+    const [data, setData] = useState("null");
+
+    useEffect(() => {
+        getStatus()
+    }, []);
+
+    const getStatus = async() =>{
+      axios.get('http://127.0.0.1:8101/status')
+        .then(response => {
+            setData(response.data);
+            //console.log(data.status)
+        })
+        .catch(error => {
+            console.error('There was an error fetching the data!', error);
+        });
+    }
+      
+    return (
+        <div>
+            <h1>React and Flask</h1>
+            {data ? <p>Status: {data.status}</p> : <p>Loading...</p>}
+        </div>
+    );
+}
+
+export default App;
