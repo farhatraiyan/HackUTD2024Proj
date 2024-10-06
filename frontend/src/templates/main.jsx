e1597c5ee67c6efedccc713df023b8db50d75aa3
Farhat R. Kabir <148591416+farhatraiyan@users.noreply.github.com>
2024-10-06 05:59:46 -0500

    templates directory

--- frontend/src/templates/main.jsx
+++ frontend/src/templates/main.jsx
@@ -1,9 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './components/App.jsx'
-import Navbar from './components/Navbar.jsx'
-import './index.css'
-import './App.css';
+import App from '../components/App.jsx'
+import Navbar from '../components/Navbar.jsx'
+import '../index.css'
+import '../App.css';
+import '../navbar.css';
 
 createRoot(document.getElementById('navbar')).render(
   <StrictMode>
