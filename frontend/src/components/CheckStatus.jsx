464555d54e80d71ab7f8cfe4b117b6c24c836619
Farhat R. Kabir <148591416+farhatraiyan@users.noreply.github.com>
2024-10-07 05:04:41 -0500

    views directory full implementation
    also optimized status component

--- frontend/src/components/CheckStatus.jsx
+++ frontend/src/components/CheckStatus.jsx
@@ -1,28 +1,22 @@
-import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 
 function checkStatus() {
-    const [data, setData] = useState("null");
+    const [data, setData] = useState({ status: 'loading' });
 
     useEffect(() => {
         getStatus()
     }, []);
 
     const getStatus = async() =>{
-      axios.get('http://127.0.0.1:8101/status')
-        .then(response => {
-            setData(response.data);
-            //console.log(data.status)
-        })
-        .catch(error => {
-            console.error('There was an error fetching the data!', error);
-        });
+        const response = await fetch('http://127.0.0.1:8101/status', { method: 'GET' });
+
+        setTimeout(async () => setData(await response.json()), 1000);
     }
       
     return (
         <div>
             <h1>Current Status</h1>
-            {data ? <p>Status: {data.status}</p> : <p>Loading...</p>}
+            <p>Status: {data.status}</p>
         </div>
     );
 }
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -3,10 +3,10 @@ import React from 'react';
 function Navbar() { 
     return (
         <ul>
-            <li><a href = "/">Home</a></li>
-            <li><a href = "/src/views/create/">Create Account</a></li>
-            <li><a href = "/src/views/status/">Status</a></li>
-            <li><a href = "/src/views/about/">About</a></li>
+            <li><a href = "/views/home/">Home</a></li>
+            <li><a href = "/views/create/">Create Account</a></li>
+            <li><a href = "/views/status/">Status</a></li>
+            <li><a href = "/views/about/">About</a></li>
         </ul>
     );
 }
