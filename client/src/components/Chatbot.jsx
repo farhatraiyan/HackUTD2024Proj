d90392ba9a73fb748fddb3a4c4d8e9f26032a73b
Farhat R. Kabir <148591416+farhatraiyan@users.noreply.github.com>
2024-10-17 20:59:44 -0500

    Chatbot.jsx cleanup

--- client/src/components/Chatbot.jsx
+++ client/src/components/Chatbot.jsx
@@ -16,16 +16,19 @@ function AI() {
   
     const onClick = async (e) => {
         e.preventDefault();
+
         if(userPrompt == "")
             return;
+
         setLoading(true);
+
         let msgs = messages;
         msgs.push({ role: "user", content: userPrompt });
+
         setMessages(msgs);
         setUserPrompt("");
 
         const url = image ? "/ai?image=true" : "/ai";
-
         const options = {
             method: "POST",
             headers: {
@@ -33,45 +36,38 @@ function AI() {
             },
             body: JSON.stringify(userPrompt)
         }
+
         const response = await fetch(`${server_url}${url}`, options);
         const data = await response.json();
-        console.log(data)
+
         msgs.push(data);
         setMessages(msgs);
-        console.log(messages);
         setLoading(false);
     }
+
     const scroll = () => {
         setTimeout(scrollTo(0, 1e10), 10000); 
     }
 
     //scrolls to the bottom of the page everytime a value changes
     useEffect(() => {scroll()});
-//center is the main text area, and bottom is where the textbox is.
-//the messages.map maps the messages array to ai message and user message.
+
+    //center is the main text area, and bottom is where the textbox is.
+    //the messages.map maps the messages array to ai message and user message.
     return (
-        
         <div className = 'chat'>
             <div className="center">
-            <div className="message">
-                    <div className="texts">
-                        <p>Type your message</p>
-                    </div>
-            </div>
-            {messages && messages.length
-            ? messages.map((chat, index) => (
-              <p key={index} className={chat.role === "user" ? "user_msg" : ""}>
-                {/* Checks if the ai returned an image link or not. If it did, it sends the image as a response, and if not, it returns the ai's text response*/}
-                <span className={chat.role === "user" ? "user_msg" : ""}><img src = {chat.content.substring(0, 16) === "https://oaidalle" ?  chat.content : ""} onError = {chat.content}/>{chat.content.substring(0, 16) === "https://oaidalle" ?  "" : chat.content}</span>
-              </p>
-            ))
-          : ""}
-
-            <div className={loading ? "" : "hide"}>
-                <p>
-                    <i>{loading ? "Thinking" : ""}</i>
-                </p>
-            </div>
+                {messages && messages.length ? messages.map((chat, index) => (
+                    <p key={index} className={chat.role === "user" ? "user_msg" : ""}>
+                        {/* Checks if the ai returned an image link or not. If it did, it sends the image as a response, and if not, it returns the ai's text response*/}
+                        <span className={chat.role === "user" ? "user_msg" : ""}><img src = {chat.content.substring(0, 16) === "https://oaidalle" ?  chat.content : ""} onError = {chat.content}/>{chat.content.substring(0, 16) === "https://oaidalle" ?  "" : chat.content}</span>
+                    </p>
+                )) : ""}
+                <div className={loading ? "" : "hide"}>
+                    <p>
+                        <i>{loading ? "Thinking" : ""}</i>
+                    </p>
+                </div>
             </div>
             <form onSubmit={onClick}>
                 <div className="bottom">
@@ -79,12 +75,10 @@ function AI() {
                     <button type = "button" className={image == true ? 'imageOn': 'imageOff'} onClick={() => setImage(!image)}></button>
                     <input type='text' placeholder='Type your message...' value={userPrompt} onChange={(e) => setUserPrompt(e.target.value)}/>
                     <button className='sendButton' disabled={loading}>Send</button>
-                
                 </div>
             </form>
         </div>
-        
-    )
+    );
 };
 
 export default AI;
