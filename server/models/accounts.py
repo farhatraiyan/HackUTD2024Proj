8ec32c90f6d7ae9e723ce8b820f763b1c0f2f5a1
Farhat R. Kabir <148591416+farhatraiyan@users.noreply.github.com>
2024-10-31 01:56:39 -0500

    Update accounts.py

--- server/models/accounts.py
+++ server/models/accounts.py
@@ -1,14 +1,15 @@
 from appDB import db
-from sqlalchemy.dialects.sqlite import JSON
+from sqlalchemy import String
+import uuid
 
 class Account(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
     username = db.Column(db.String(40), unique=True, nullable=False)
     password = db.Column(db.String(20), nullable=False)
 
     def __init__(self, username, password):
-            self.username = username
-            self.password = password
+        self.username = username
+        self.password = password
 
     def to_dict(self):
         return {
