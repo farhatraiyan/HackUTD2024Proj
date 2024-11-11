bebbb38eff2f1b4a934ab3f0f91d910c53c1923f
Farhat R. Kabir <148591416+farhatraiyan@users.noreply.github.com>
2024-11-11 05:22:07 -0600

    cleanup + comments

--- server/app.py
+++ server/app.py
@@ -1,26 +1,34 @@
+# Import DB and DB initializer
 from appDB import create_app, db
-from flask_restful import Api
-from flask_cors import CORS
-import os
 
-from controllers.utils import Status
+# Import Flask API for AI stuff (probably will remove soon)
+from flask_restful import Api
+# Import Flask CORS to handle Frontend requests
+from flask_cors import CORS
+
+# Import AIModel
 from controllers.ai import AIModel
 
+# Import Blueprints (app routes are defined within these)
 from routes.accounts import accounts_bp
 from routes.pages import pages_bp
 
+# Initialize Flask app, API, and CORS, DB
 app = create_app()
 api = Api(app)
 CORS(app)
 
+# Create DB tables if they don't exist
 with app.app_context():
     db.create_all()
 
-api.add_resource(Status, '/status')
+# Add AIModel to API (probably will remove soon)
 api.add_resource(AIModel, '/ai')
 
+# Register route Blueprints
 app.register_blueprint(accounts_bp)
 app.register_blueprint(pages_bp)
 
+# Run the app on port 8101
 if __name__ == '__main__':
     app.run(debug=True, port=8101)
