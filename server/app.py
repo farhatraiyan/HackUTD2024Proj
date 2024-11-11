5c2624e403e5350df41e957cc950df33a7589004
Farhat R. Kabir <148591416+farhatraiyan@users.noreply.github.com>
2024-11-11 05:17:10 -0600

    page routes delegation

--- server/app.py
+++ server/app.py
@@ -1,13 +1,13 @@
 from appDB import create_app, db
-
-from flask import jsonify, request, send_from_directory
 from flask_restful import Api
 from flask_cors import CORS
 import os
 
-from routes.accounts import accounts_bp
 from controllers.utils import Status
 from controllers.ai import AIModel
+
+from routes.accounts import accounts_bp
+from routes.pages import pages_bp
 
 app = create_app()
 api = Api(app)
@@ -16,37 +16,11 @@ CORS(app)
 with app.app_context():
     db.create_all()
 
-def serve_page(page):
-    return send_from_directory(os.path.join(app.root_path, 'dist/views'), page)
-
 api.add_resource(Status, '/status')
 api.add_resource(AIModel, '/ai')
 
 app.register_blueprint(accounts_bp)
-
-@app.route('/')
-def serve_home():
-    return serve_page('home.html')
-
-@app.route('/create')
-def serve_create_acc():
-    return serve_page('create.html')
-
-@app.route('/checkStatus')
-def serve_status():
-    serve_page('status.html')
-
-@app.route('/about')
-def serve_about():
-    return serve_page('about.html')
-
-@app.route('/chatbot')
-def serve_chatbot():
-    return serve_page('chatbot.html')
-
-@app.route('/assets/<path:filename>')
-def serve_assets(filename):
-    return send_from_directory(os.path.join(app.root_path, 'dist/assets'), filename)
+app.register_blueprint(pages_bp)
 
 if __name__ == '__main__':
     app.run(debug=True, port=8101)
---
+++ server/routes/pages.py
@@ -0,0 +1,34 @@
+from flask import Blueprint, send_from_directory
+from pathlib import Path
+import os
+
+pages_bp = Blueprint('pages', __name__)
+
+def serve_page(page):
+    current_dir = Path(__file__).parent.parent
+    return send_from_directory(os.path.join(current_dir, 'dist/views'), page)
+
+@pages_bp.route('/')
+def serve_home():
+    return serve_page('home.html')
+
+@pages_bp.route('/create')
+def serve_create_acc():
+    return serve_page('create.html')
+
+@pages_bp.route('/checkStatus')
+def serve_status():
+    return serve_page('status.html')
+
+@pages_bp.route('/about')
+def serve_about():
+    return serve_page('about.html')
+
+@pages_bp.route('/chatbot')
+def serve_chatbot():
+    return serve_page('chatbot.html')
+
+@pages_bp.route('/assets/<path:filename>')
+def serve_assets(filename):
+    current_dir = Path(__file__).parent.parent
+    return send_from_directory(os.path.join(current_dir, 'dist/assets'), filename)
