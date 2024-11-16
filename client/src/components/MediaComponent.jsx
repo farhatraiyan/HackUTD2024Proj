279c65d928e1bdd58b0ff1fb3375596df4c469e8
Farhat R. Kabir <148591416+farhatraiyan@users.noreply.github.com>
2024-11-15 23:56:18 -0600

    frontend image and preview separation

--- client/src/components/MediaComponent.jsx
+++ client/src/components/MediaComponent.jsx
@@ -5,10 +5,11 @@ import { useState } from "react";
 export function ViewMedia() {
     const [cid, setCid] = useState("");
     const [imageUrl, setImageUrl] = useState('');
+    const [previewUrl, setPreviewUrl] = useState('');
 
     const getMedia = async cid => {
         try {
-            const response = await fetch(`/media/${cid}`, { method: "GET" });
+            const response = await fetch(`/media/${cid}?original=true`, { method: "GET" });
             if (!response.ok) throw new Error(`HTTP error: ${response.status}`);
     
             const blob = await response.blob();
@@ -20,19 +21,45 @@ export function ViewMedia() {
         }
     };
 
+    const getPreview = async cid => {
+        try {
+            const response = await fetch(`/media/${cid}?original=false`, { method: "GET" });
+            if (!response.ok) throw new Error(`HTTP error: ${response.status}`);
+    
+            const blob = await response.blob();
+            const imageUrl = URL.createObjectURL(blob);
+            setPreviewUrl(imageUrl);
+        } catch (error) {
+            alert("Upload failed:" + error);
+            throw error;
+        }
+    };
+
     return (
         <div className="w-full max-w-xs">
-            {imageUrl && (
-                <div className="mt-4">
-                    <img src={imageUrl} alt="Preview" />
-                </div>
-            )}
+            <div className=" outline-dashed">
+                {imageUrl && (
+                    <div className="mt-4">
+                        <img src={imageUrl} alt="Preview" />
+                    </div>
+                )}
+            </div>
+            <div className="outline-double">
+                {previewUrl && (
+                    <div className="mt-4">
+                        <img src={previewUrl} alt="Preview" />
+                    </div>
+                )}
+            </div>
             <form
                 className="flex flex-col gap-2"
                 onSubmit={async e => {
                     e.preventDefault();
                     if (cid) {
-                        await getMedia(cid);
+                        await Promise.all([
+                            getMedia(cid),
+                            getPreview(cid)
+                        ]);
                     }
                 }}
             >
