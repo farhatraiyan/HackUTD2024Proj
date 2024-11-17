c3ffaa341137dd0323a46920a37beb9d41933fca
Farhat R. Kabir <148591416+farhatraiyan@users.noreply.github.com>
2024-11-17 08:34:05 -0600

    Update Navbar.jsx

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -14,7 +14,6 @@ export default function NavbarComponent() {
                 <Navbar.Link className="text-white" href="/">Home</Navbar.Link>
                 <Navbar.Link className="text-white" href="/media">Media</Navbar.Link>
                 <Navbar.Link className="text-white" href="/upload">Upload</Navbar.Link>
-                <Navbar.Link className="text-white" href="/about">About</Navbar.Link>
                 <Navbar.Link className="text-white" href={loggedIn? "/user":"/signin"}>Account</Navbar.Link>
             </Navbar.Collapse>
         </Navbar>
